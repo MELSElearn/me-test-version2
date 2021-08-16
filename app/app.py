896a61230123815f1e0f03efb107723d290f7dba
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2021-08-16 20:50:19 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,8 +38,8 @@ def hello():
         make_picture('app/housing_small.csv', model, np_arr, path)
         
         
-        #return render_template('index.html', href=path, href2='The suitable house for values ('+ text +')' +' is:'+predictions_to_str)
-        return render_template('index.html', href='static/base_pic.svg', href2='The suitable house for values ('+ text +')' +' is:'+predictions_to_str)
+        return render_template('index.html', href=path, href2='The suitable house for values ('+ text +')' +' is:'+predictions_to_str)
+        #return render_template('index.html', href='static/base_pic.svg', href2='The suitable house for values ('+ text +')' +' is:'+predictions_to_str)
 
 def make_picture(training_data_filename,model,new_input_arr, output_file):
     housing_data = pd.read_csv(training_data_filename)
@@ -59,23 +59,7 @@ def make_picture(training_data_filename,model,new_input_arr, output_file):
     plt.tight_layout()
     plt.savefig(output_file)
     plt.show()
-    
-    
-    #preds = model.predict(incomes.values.reshape(-1,1))
 
-    #fig = px.scatter(x=incomes, y=housevalues, title="Income vs House value", labels={'x': 'Income (10k)',
-                                                                                    'y': 'House value'})
-
-    #fig.add_trace(go.Scatter(x=incomes.values.reshape(-1,1), y=preds, mode='lines', name='Model'))
-
-    #new_preds = model.predict(new_inp_np_arr)
-
-    #fig.add_trace(go.Scatter(x=new_inp_np_arr.reshape(len(new_inp_np_arr)), y=new_preds, name='New Outputs', mode='markers', marker=dict(color='purple', size=20, line=dict(color='purple', width=2))))
-
-    #fig.write_image(output_file, width=800, engine='kaleido')
-    #fig.show()
-    
-    
 def floats_string_to_np_arr(floats_str):
   def is_float(s):
     try:
