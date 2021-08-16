f97f4db74fbe2645c8e1f0e5fd956f3e1eb1dbf3
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2021-08-16 19:27:08 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,54 +1,67 @@
 from flask import Flask, render_template, request
 import numpy as np
-from joblib import load
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+from joblib import load
 import uuid
+import matplotlib.pyplot as plt
+#from sklearn.linear_model import LinearRegression
+#import math
 import os
 
 app = Flask(__name__)
+#run_with_ngrok(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+#@app.route('/entry', methods=['GET', 'POST'])
+def hello():
     request_type_str = request.method
+    
     if request_type_str == 'GET':
-        return render_template('index.html', href='static/base_pic.svg')
+        return render_template('index.html', href='static/base_pic.svg', href2='')
     else:
+        
+        
         text = request.form['text']
         random_string = uuid.uuid4().hex
-        path = "app/static/" + random_string + ".svg"
-        files = [f for f in os.listdir('.') if os.path.isfile(f)]
-        for f in files:
-          print(f)
-        model = load('app/model.joblib')
+        path = "static/" + random_string + ".svg"
+        model = load('HousingLinear.joblib')
         np_arr = floats_string_to_np_arr(text)
-        make_picture('app/AgesAndHeights.pkl', model, np_arr, path)
+        predictions = model.predict(np_arr.reshape(-1,1))
+           
+        #list1 = predictions.tolist()
+        #list1_str = ''
+        #for x in list1:
+        #    list1_str = list1_str + str(math.trunc(x)) + ','
 
-        return render_template('index.html', href=path[4:])
+        predictions_to_str = str(predictions)
+        
+        make_picture('housing_small.csv', model, np_arr, path)
+        
+        #return predictions_to_str
+        return render_template('index.html', href=path, href2='The suitable house for values ('+ text +')' +' is:'+predictions_to_str)
 
 
-def make_picture(training_data_filename, model, new_inp_np_arr, output_file):
-  data = pd.read_pickle(training_data_filename)
-  ages = data['Age']
-  data = data[ages > 0]
-  ages = data['Age']
-  heights = data['Height']
-  x_new = np.array(list(range(19))).reshape(19, 1)
-  preds = model.predict(x_new)
+def make_picture(training_data_filename,model,new_input_arr, output_file='predictions_pic.svg'):
+    housing_data = pd.read_csv(training_data_filename)
+    incomes = housing_data['median_income']
+    housevalues = housing_data['median_house_value'] 
 
-  fig = px.scatter(x=ages, y=heights, title="Height vs Age of People", labels={'x': 'Age (years)',
-                                                                                'y': 'Height (inches)'})
-
-  fig.add_trace(go.Scatter(x=x_new.reshape(19), y=preds, mode='lines', name='Model'))
-
-  new_preds = model.predict(new_inp_np_arr)
-
-  fig.add_trace(go.Scatter(x=new_inp_np_arr.reshape(len(new_inp_np_arr)), y=new_preds, name='New Outputs', mode='markers', marker=dict(color='purple', size=20, line=dict(color='purple', width=2))))
-  
-  fig.write_image(output_file, width=800, engine='kaleido')
-  fig.show()
-
+    predictions = model.predict(incomes.values.reshape(-1,1))
+    plt.plot(incomes, predictions, label = 'Linear Regression', color = 'b', alpha = .7)
+    plt.scatter(incomes, housevalues, label = 'Actual Test Data', color ='g', alpha = .7)
+    plt.legend
+    plt.xlabel('median income(10k)')
+    plt.ylabel('median house value')
+    
+    
+    new_pred = model.predict(new_input_arr)
+    plt.scatter(new_input_arr, new_pred, label = 'Actual Test Data', color ='r', alpha = .7)
+    
+    plt.tight_layout()
+    plt.savefig(output_file)
+    plt.show()
+    
+    
 def floats_string_to_np_arr(floats_str):
   def is_float(s):
     try:
